57a54ac37c4e49537e06624e12a93582cbdf80ca
Karina Valens <karyvalens@yahoo.com>
2022-08-28 22:05:01 +0200

    clear botton working

--- script.js
+++ script.js
@@ -2,56 +2,55 @@
 //window.addEventListener("load", start); //init calculator
 
 const calc = document.querySelector("#calculate");
-document.querySelector("#calculate").addEventListener("click", calculator);
 let dropdown = document.querySelector("#decimals").value;
-
 let n1;
 let n2;
 let oper;
 let result;
-
-
+document.querySelector("#calculate").addEventListener("click", calculator);
+document.querySelector("#clear").addEventListener("click", clear);
 
 function calculator() {
     n1 = parseInt(document.querySelector("#firstnumber").value); //parseInt makes sure the input is converted to a number
     n2 = parseInt(document.querySelector("#secondnumber").value);
     oper = document.querySelector("#operator").value;
-    //assigning the operations
-    if (oper === "add") {
-        result = document.querySelector("#firstnumber").value = n1 + n2;
-    }
-    if (oper === "sub") {
-        result = document.querySelector("#firstnumber").value = n1 - n2;
-    }
-    if (oper === "mul") {
-        result = document.querySelector("#firstnumber").value = n1 * n2;
-    }
-    if (oper === "div") {
-        result = document.querySelector("#firstnumber").value = n1 / n2;
+    if (oper === "add") { //assigning the operations
+        result = value = n1 + n2;
+    } else if (oper === "sub") {
+        result = value = n1 - n2;
+    } else if (oper === "mul") {
+        result = value = n1 * n2;
+    } else if (oper === "div") {
+        result = value = n1 / n2;
     }
     roundRes();
-
 }
 
 function roundRes() {
     let dec = document.querySelector("#decimals").value;
     let round = document.querySelector("#doround");
-    //console.log(round.checked);
-    //console.log(result);
-    //console.log(dec);
-    if (round.checked === true) {
-        result = result.toFixed(dec);
+    if (round.checked === true) { // read from drop-down if it is checked
+        result = result.toFixed(dec); //to round the result accordingly the chosen number from the drop-down
     }
+    document.querySelector("#firstnumber").value = result;
     results();
 }
 
 function results() {
-    document.querySelector("#results").innerHTML += result;
+    document.querySelector("#results li:last-child").innerHTML = result;
+    //document.querySelector("#results").scroll(0, 1000); //this is intendet to print the result in the <li> but does not work
+}
+
+function clear() {
+    document.querySelector("#firstnumber").value = "";
+    document.querySelector("#secondnumber").value = "";
+    document.querySelector("#results").innerHTML = "";
+    document.querySelector("#decimals").value = "";
+    document.querySelector("#doround").checked = false;
+    document.querySelector("html").offsetHeight;
 }
 
 
-// writing to field = 
 
-// read from drop-down same as from field.value
-// cheching check box  not .value but .checked=>boolean
-// scrolling = document.querySelector("#results").scroll to (0.100000)=>any large number 
+
+
